606b54d9543b0aa758e0b6e19ae8f62f58b34dd5
Chase Douglas <chase@citylinecreative.com>
2024-02-29 12:26:07 -0600

    added a settings page in the plugin that will be added to later. Right now it is just a simple checkbox to turn on/off the plugin being active

---
+++ email-obfuscator-settings.php
@@ -0,0 +1,84 @@
+<?php
+/*
+Plugin Name: Email Obfuscator Settings 
+Description: Simple settings panel do enable or disable the code from running. More features coming soon.
+Version: 1.0
+Author: Chase Douglas
+*/
+
+// Hook into the admin menu to add the settings page
+add_action('admin_menu', 'email_obfuscator_add_admin_menu');
+function email_obfuscator_add_admin_menu()
+{
+    add_options_page('Email Obfuscator Settings', 'Email Obfuscator', 'manage_options', 'email_obfuscator', 'email_obfuscator_settings_page');
+}
+
+// Render the settings page content
+function email_obfuscator_settings_page()
+{
+?>
+    <div class="wrap">
+        <h2>Email Obfuscator Settings</h2>
+        <form action="options.php" method="post">
+            <?php
+            settings_fields('email_obfuscator_options_group');
+            do_settings_sections('email_obfuscator');
+            submit_button();
+            ?>
+        </form>
+    </div>
+<?php
+}
+
+// Initialize settings
+add_action('admin_init', 'email_obfuscator_settings_init');
+function email_obfuscator_settings_init()
+{
+    register_setting('email_obfuscator_options_group', 'email_obfuscator_options', 'email_obfuscator_options_sanitize');
+
+    add_settings_section(
+        'email_obfuscator_settings_section',
+        'General Settings',
+        'email_obfuscator_settings_section_callback',
+        'email_obfuscator'
+    );
+
+    add_settings_field(
+        'email_obfuscator_setting_enable',
+        'Enable Email Obfuscation',
+        'email_obfuscator_setting_enable_render',
+        'email_obfuscator',
+        'email_obfuscator_settings_section'
+    );
+}
+
+function email_obfuscator_settings_section_callback()
+{
+    echo 'Customize the Email Obfuscator settings below.';
+}
+
+function email_obfuscator_setting_enable_render()
+{
+    $options = get_option('email_obfuscator_options');
+?>
+    <input type='checkbox' name='email_obfuscator_options[email_obfuscator_setting_enable]' <?php checked(isset($options['email_obfuscator_setting_enable']), 1); ?> value='1'>
+<?php
+}
+
+// Sanitize and validate input
+function email_obfuscator_options_sanitize($options)
+{
+    if (!is_array($options)) {
+        $options = [];
+    }
+
+    if (isset($options['email_obfuscator_setting_enable'])) {
+        // Ensure the input is a boolean value
+        $options['email_obfuscator_setting_enable'] = (bool)$options['email_obfuscator_setting_enable'];
+    } else {
+        // Default to false if not set
+        $options['email_obfuscator_setting_enable'] = false;
+    }
+
+    return $options;
+}
--- email-obfuscator.php
+++ email-obfuscator.php
@@ -10,55 +10,65 @@ License:     GPL2
 License URI: https://www.gnu.org/licenses/gpl-2.0.html
 */
 
-// Code to hide emails from bots
-function encrypt_email($email)
+include_once(plugin_dir_path(__FILE__) . 'email-obfuscator-settings.php');
+
+
+function email_obfuscator_init()
 {
-    return base64_encode($email);
-}
-
-function generate_decrypt_script($encrypted_email)
-{
-    return 'DeCryptX(\'' . $encrypted_email . '\')';
-}
-
-function generate_mailto_link($email)
-{
-    $encrypted_email = encrypt_email($email);
-    $javascript_code = generate_decrypt_script($encrypted_email);
-    // Ensure only the JavaScript call is being used, without additional HTML nesting
-    return '<a href="javascript:' . htmlspecialchars($javascript_code) . '">' . htmlspecialchars($email) . '</a>';
-}
-
-function obfuscate_emails($content)
-{
-    // Create a new DOMDocument and load the content
-    $dom = new DOMDocument();
-    @$dom->loadHTML(mb_convert_encoding($content, 'HTML-ENTITIES', 'UTF-8'), LIBXML_HTML_NOIMPLIED | LIBXML_HTML_NODEFDTD);
-    $links = $dom->getElementsByTagName('a');
-
-    foreach ($links as $link) {
-        // Check if the link is a mailto link
-        $href = $link->getAttribute('href');
-        if (strpos($href, 'mailto:') === 0) {
-            $email = substr($href, 7); // Remove the 'mailto:' part
-            $encryptedEmail = encrypt_email($email);
-            $decryptedLink = generate_decrypt_script($encryptedEmail);
-
-            // Set the new href to call the JavaScript decrypt function
-            $link->setAttribute('href', 'javascript:' . $decryptedLink);
-            // Optionally, set the display text to the encrypted email or leave it as is
-            // $link->nodeValue = $encryptedEmail;
+    $options = get_option('email_obfuscator_options');
+    if (!empty($options['email_obfuscator_setting_enable'])) {
+        // Code to hide emails from bots
+        function encrypt_email($email)
+        {
+            return base64_encode($email);
         }
+
+        function generate_decrypt_script($encrypted_email)
+        {
+            return 'DeCryptX(\'' . $encrypted_email . '\')';
+        }
+
+        function generate_mailto_link($email)
+        {
+            $encrypted_email = encrypt_email($email);
+            $javascript_code = generate_decrypt_script($encrypted_email);
+            // Ensure only the JavaScript call is being used, without additional HTML nesting
+            return '<a href="javascript:' . htmlspecialchars($javascript_code) . '">' . htmlspecialchars($email) . '</a>';
+        }
+
+        function obfuscate_emails($content)
+        {
+            // Create a new DOMDocument and load the content
+            $dom = new DOMDocument();
+            @$dom->loadHTML(mb_convert_encoding($content, 'HTML-ENTITIES', 'UTF-8'), LIBXML_HTML_NOIMPLIED | LIBXML_HTML_NODEFDTD);
+            $links = $dom->getElementsByTagName('a');
+
+            foreach ($links as $link) {
+                // Check if the link is a mailto link
+                $href = $link->getAttribute('href');
+                if (strpos($href, 'mailto:') === 0) {
+                    $email = substr($href, 7); // Remove the 'mailto:' part
+                    $encryptedEmail = encrypt_email($email);
+                    $decryptedLink = generate_decrypt_script($encryptedEmail);
+
+                    // Set the new href to call the JavaScript decrypt function
+                    $link->setAttribute('href', 'javascript:' . $decryptedLink);
+                    // Optionally, set the display text to the encrypted email or leave it as is
+                    // $link->nodeValue = $encryptedEmail;
+                }
+            }
+
+            // Save the modified content
+            return $dom->saveHTML();
+        }
+        add_filter('the_content', 'obfuscate_emails');
+
+
+        function email_obfuscator_enqueue_scripts()
+        {
+            wp_enqueue_script('email-decrypt', plugin_dir_url(__FILE__) . 'js/decrypt.js', array(), '1.0', true);
+        }
+        add_action('wp_enqueue_scripts', 'email_obfuscator_enqueue_scripts');
     }
-
-    // Save the modified content
-    return $dom->saveHTML();
 }
-add_filter('the_content', 'obfuscate_emails');
-
-
-function email_obfuscator_enqueue_scripts()
-{
-    wp_enqueue_script('email-decrypt', plugin_dir_url(__FILE__) . 'js/decrypt.js', array(), '1.0', true);
-}
-add_action('wp_enqueue_scripts', 'email_obfuscator_enqueue_scripts');
+add_action('init', 'email_obfuscator_init');
